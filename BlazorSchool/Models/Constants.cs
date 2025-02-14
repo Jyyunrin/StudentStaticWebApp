45a9a54ad82c1b42576eddc87098293195601401
Joe Lin <jlin249@my.bcit.ca>
2025-02-14 14:57:53 -0800

    Modified base url

--- BlazorSchool/Models/Constants.cs
+++ BlazorSchool/Models/Constants.cs
@@ -4,6 +4,6 @@ namespace BlazorSchool.Models;
 
 public class Constants
 {
-    public const string BASE_URL = "https://nice-mud-048739700.4.azurestaticapps.net/";
+    public const string BASE_URL = "";
 }
 
